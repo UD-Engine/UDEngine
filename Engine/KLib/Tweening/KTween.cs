0e3a8769a6f1939441f3e8c931621f5f0a0316ac
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2017-09-05 15:33:01 +0800

    Recommit for losing something

---
+++ Engine/KLib/Tweening/KTween.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+namespace KSM.Tweening {
+
+	/// <summary>
+	/// Tweened float. Useful for time interval changes (e.g. for yield return new WaitForSeconds)
+	/// </summary>
+	public class KTweenedFloat {
+		public KTweenedFloat(float start, float end, float time) {
+			this.startValue = start;
+			this.value = start;
+			this.endValue = end;
+			this.interval = time;
+		}
+
+		public float value;
+
+		public float startValue;
+		public float endValue;
+		public float interval;
+
+		public Ease easeType;
+		private bool _isEaseSet = false;
+
+		public float GetValue() {
+			return this.value;
+		}
+
+		public KTweenedFloat SetEase(Ease easeType) {
+			this.easeType = easeType;
+			_isEaseSet = true;
+			return this;
+		}
+
+		public KTweenedFloat Run() {
+			if (!_isEaseSet) {
+				easeType = DOTween.defaultEaseType;
+			}
+
+			DOTween.To (() => this.value, x => this.value = x, this.endValue, this.interval).SetEase(this.easeType);
+			return this;
+		}
+	}
+
+}
---
+++ Example/Test/TestDOTween/TimeTweening/TestDOTweenTimeTweening.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using DG.Tweening;
+using KSM.Tweening;
+
+public class TestDOTweenTimeTweening : MonoBehaviour {
+
+	public Text txt;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (TestCoroutine ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator TestCoroutine() {
+		KTweenedFloat interval = new KTweenedFloat (0.1f, 0.01f, 5f);
+
+		interval.SetEase (Ease.Linear).Run ();
+
+		while (true) {
+			txt.text = txt.text + "1";
+
+			yield return new WaitForSeconds (interval.GetValue());
+		}
+
+		yield break;
+	}
+}
